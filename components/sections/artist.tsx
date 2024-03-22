e40448c24034958b09eec5fcfa2f1cc2ba43adfc
vojmax <vojislav.maksovic@gmail.com>
2024-03-22 21:08:53 +0100

    added artist info and avatar

--- components/sections/artist.tsx
+++ components/sections/artist.tsx
@@ -4,8 +4,14 @@ import { ImageType } from "@/lib/types";
 
 export default async function Artist({ image }: { image: ImageType }) {
   return (
-    <section className="z-10 w-full h-64 bg-gradient-to-t from-indigo-950/0 to-indigo-950/60 justify-center items-center text-white">
-      <div>
+    <section className="flex z-10 gap-10 w-full h-64 bg-gradient-to-t from-indigo-950/0 to-indigo-950/60 justify-center text-white ">
+      <div className="mt-10">
+        <Avatar className="w-20 h-20">
+          <AvatarImage src={image.user.profile_image.large} />
+          <AvatarFallback>{image.user.name.slice(0, 1)}</AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="mt-10">
         <h1>{image.alt_description}</h1>
         <h2>{image.user.name}</h2>
         <a>{image.user.social.portfolio_url}</a>
