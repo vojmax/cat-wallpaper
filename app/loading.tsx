1ebdf473c53231ba401c92e40feee126af946813
vojmax <vojislav.maksovic@gmail.com>
2024-03-23 21:15:45 +0100

    styling changed and added icon for download

--- app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex relative min-h-screen flex-col items-center justify-between bg-slate-950">
-      <p>loging</p>
-    </div>
-  );
-}
